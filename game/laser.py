6c2ee144e5a186d8791bc0af94047fc3515d39ee
Ramez <ramez.festek@gmail.com>
2025-10-14 19:12:41 -0400

    assigned the L & R arrow keys to control the x axis movement of the 'player'. Set 'HeroShip' class from spaceship file = to Player. Next step will be to add tests to see if this is functioning. Still receiving terminal err msg when trying to run main game loop.

--- game/laser.py
+++ game/laser.py
@@ -5,7 +5,7 @@ import pygame
 class Laser(pygame.sprite.Sprite):
     def __init__(self, position, speed):
         super().__init__()
-        self.image = pygame.Surface((4, 15))
+        self.image = pygame.Surface((4, 20))
         self.image.fill((243, 216, 63))
         self.rect = self.image.get_rect(center=position)
         self.speed = speed
--- game/player.py
+++ game/player.py
@@ -1,6 +1,10 @@
+from tkinter import RIGHT
 import pygame
 import sys
 from pygame.locals import * #For useful variables
+from laser import Laser
+from HeroShip import HeroShip
+
 
 
 class Player(pygame.sprite.Sprite):
@@ -13,8 +17,51 @@ class Player(pygame.sprite.Sprite):
 		self.ready = True
 		self.laser_time = 0
 		self.laser_cooldown = 600
-
 		self.lasers = pygame.sprite.Group()
-
 		self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
 		self.laser_sound.set_volume(0.5)
+
+HeroShip = Player(HeroShip.rect.center,HeroShip.max_x_constraint,HeroShip.speed)
+cool_down_time = 600 # just over half a sec cool down between shots
+Last_shot_time = 0
+running = True
+
+
+def get_input (self):
+	keys = pygame.key.get_pressed()
+# right/left navigation thru arrow keys
+	if keys[pygame.K_RIGHT]: 
+		self.rect.x += self.speed
+	elif keys[pygame.K_INSERT]:
+		self.rect.x -= self.speed
+# spacebar to shoot laser
+	if keys[pygame.K_space]:
+		self.shoot_laser()
+		self.ready = False
+		self.recharge_time = pygame.time.get_ticks()
+		self.laser_sound.play()
+
+		def recharge(self): 
+			if not self.ready:
+				current_time = pygame.time.get_ticks()
+				if current_time - self.recharge_time >= self.cool_down_time:
+					self.ready = True
+# 'margin' comnstraints for setting boundaries for where the player can move to 
+def constraint(self):
+	if self.rect.left <= 0:
+		self.rect.left = 0
+	if self.rect.right >= self.max_x_constraint:
+		self.rect.right = self.max_x_constraint
+        
+def shoot_laser(self):
+	self.lasers.add(Laser(self.rect.center,-8,self.rect.bottom))
+
+def update(self):
+	self.get_input()
+	self.constraint()
+	self.recharge()
+	self.lasers.update()
+
+
+		
+		
