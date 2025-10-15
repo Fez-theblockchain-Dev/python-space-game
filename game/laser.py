4828b3cfcb8311c56915f2b5696d812c8c647228
Ramez <ramez.festek@gmail.com>
2025-10-15 00:22:05 -0400

    added the 'config.py' file for all shared imports and added a destroy function method to destroy/delete the lasers when they hit the edge of the screen.

--- game/laser.py
+++ game/laser.py
@@ -3,15 +3,21 @@ import pygame
 # from game import spaceship
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, position, speed):
+    def __init__(self, position, speed, screen_height):
         super().__init__()
         self.image = pygame.Surface((4, 20))
         self.image.fill((243, 216, 63))
         self.rect = self.image.get_rect(center=position)
         self.speed = speed
+        self.screen_y_axis = screen_height
+
+    def destroy(self):
+        if self.rect.y <= -50 or self.rect.y >= self.screen_y_axis + 50:
+            self.kill()
 
     def update(self):
-        self.rect.y -= self.speed
+        self.rect.y += self.speed
+        self.destroy()
         # Remove laser if it goes off screen
         if self.rect.bottom < 0:
             self.kill()
