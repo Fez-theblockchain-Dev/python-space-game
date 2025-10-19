e939c654ef5ed2561fa24c4c5f5b5576d79592b3
Ramez <ramez.festek@gmail.com>
2025-10-19 09:54:55 -0400

    created an 'update' function to remove aliens from game if they reach the bottom of the screen.

--- game/alien.py
+++ game/alien.py
@@ -2,17 +2,30 @@ import pygame, random
 
 from game.index import hero_ship
 
+aliens = pygame.sprite.Group()
 
 class Alien(pygame.sprite.Sprite):
-    def __init__(self, type, x, y):
+    def __init__(self, type, speed, x, y):
         super().__init__()
         self.type = type
         self.health = 100 # default health for all aliens
         path = f"assets/alien_{type}.png"
         self.image = pygame.image.load(path)
-        self.rect = self.image.get_rect(top_left = (x,y))
+        self.rect = self.image.get_rect(center = (x,0))
         self.value = 100 # default value for all aliens
-    
+        self.speed = speed 
+
+# function to remove the alien from game once it leaves the screen
+    def update(self):
+        self.rect.y += self.speed 
+        if self.rec.top > 600:
+            self.kill()
+
+    # Spawn timer variables
+    SPAWN_INTERVAL = 2000  # milliseconds
+    last_spawn_time = pygame.time.get_ticks()
+
+
     def alien_type(self):
         self.type_1 = 'alien_1.png'
         self.type_2 = 'alien_2.png'
