1dfb5272a5f0b9b3ec42d2e816aa0b1039c94d90
Ramez <ramez.festek@gmail.com>
2025-11-04 00:01:16 -0500

    added a new file for the 'main_menu' screen.

---
+++ game/mainMenu.py
@@ -0,0 +1,37 @@
+import pygame
+
+SCREEN = pygame.display.set_mode((1280, 720))
+
+def main_menu(): #Main menu screen
+    pygame.display.set_caption("Menu")
+
+    BG = pygame.color.load("black")
+
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
