089d5499c4df6342df0f2374f861775b94ccbd17
Ramez <ramez.festek@gmail.com>
2025-11-04 14:44:25 -0500

    created the class button module with 7 parameters to be used for the 'mainMenu' file.

---
+++ game/button.py
@@ -0,0 +1,11 @@
+class Button():
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image 
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+
+        
