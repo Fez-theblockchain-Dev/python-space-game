1a2072ba10da0e91e6af30852d19a66745e1128e
Ramez <ramez.festek@gmail.com>
2025-11-05 11:31:06 -0500

    Created a playback button to resume a new game once player losses all lives.

--- game/mainMenu.py
+++ game/mainMenu.py
@@ -1,16 +1,26 @@
+from ast import main
+import sys
 import pygame
+from button import Button
+from index import main
+from config import SCREEN_WIDTH, SCREEN_HEIGHT
 
 SCREEN = pygame.display.set_mode((1280, 720))
 BG = pygame.color.load("black")
 
-def main_menu(font): #Main menu screen
+def main_menu(self, font): #Main menu screen
     pygame.display.set_caption("Menu")
-    font = pygame.font.Font("assets/Fonts/hyperspace/Hyperspace Bold Italic.otf")
+    FONT_PATH = pygame.font.Font("assets/Fonts/hyperspace/Hyperspace Bold Italic.otf")
+    title = get_font(64).render("Space Invaders", True, "White")
+
+
+def get_font(size): #font size function
+    return pygame.font.font("assets/Fonts/hyperspace/Hyperspace Bold Italic.otf", size)
 
 
 
-    while True:
-        SCREEN.blit(BG, (0, 0))
+    # while True:
+    #     SCREEN.blit(BG, (0, 0))
 
 def play():
     while True:
@@ -24,16 +34,21 @@ def play():
 
         PLAY_BACK = Button(image=None, pos=(640, 460), 
         text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-
+        if "___main__" != True:
+            return PLAY_BACK.on_click(PLAY_MOUSE_POS)
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
+        PLAY_BACK.restart_game(SCREEN)
 
-        for event in pygame.event.get():
+for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
+            
 
-        pygame.display.update()
+pygame.display.update()
+
+sys.exit()
