f3a3a31885748e0a545b3e250bdc7487ab6f4dc5
Ramez <ramez.festek@gmail.com>
2025-12-04 00:38:47 -0500

    New game economy feature. It will tie into the ability for players to buy gold coins, health, ext.. Will look into either apple pay api integration or creating web3 Dapp + pygame if thats possible.

---
+++ gameEconomy.py
@@ -0,0 +1,15 @@
+import pygame
+import player
+
+
+class game_economy:
+    def __init__(self):
+        self.coins = 0 
+        self.score = 0 
+        self.health = player
+        self.potential_prizes = {
+            "New Avater": False,
+            "Gold Coins": False,
+            "Faster_Bullet": False
+
+        }
